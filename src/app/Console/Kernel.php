5e021861ef80607650cefbde321c70718ca1a411
Konstantin <Satana.Konst@gmail.com>
2023-03-16 12:03:20 +0300

    Добавлен нормальный запуск задания по крону

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use App\Http\Controllers\Imap;
 use App\Models\JobRules;
 use App\Models\Jobs;
+use Carbon\Carbon;
 use GuzzleHttp\Client;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -20,81 +21,93 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call(function () {
-            /**
-             * Получим список задач
-             * @var Jobs $job
-             */
-            $job = Jobs::query()->orderBy('last_run', 'desc')->first();
+            try {
+                /**
+                 * Получим список задач
+                 * @var Jobs $job
+                 */
+                $job = Jobs::query()->orderBy('last_run', 'desc')->first();
 
-            /**
-             * Бежим по правилам, фильтруем сообщения если надо и отправляем запросы
-             */
-            $imap = new Imap();
-            $imap->get();
-
-            foreach ($job->rules as $rule) {
-                $messages = $imap->getMessages();
-
-                //Фильтр по отправителю
-                if (!empty($rule->sender)) {
-                    $imap->filterEmailByFrom(
-                        $rule->sender,
-                        $messages
-                    );
+                if (empty($job)) {
+                    echo "Empty Job \n";
+                    return true;
                 }
 
-                //Фильтр по заголовку
-                if (!empty($rule->theme)) {
-                    $imap->filterEmailByTitle(
-                        $rule->theme,
-                        $messages
-                    );
-                }
+                /**
+                 * Бежим по правилам, фильтруем сообщения если надо и отправляем запросы
+                 */
+                $client = new Client();
+                $imap = new Imap();
+                $imap->get();
 
-                // Получим данные из текста сообщений
-                if (!empty($rule->regex)) {
-                    foreach ($messages as $message) {
-                        $data = [
-                            mb_strtolower($message->getSubject()->toString()),
-                            mb_strtolower($message->getFrom()->toString()),
-                            mb_strtolower($message->getTextBody()),
-                        ];
-                        $data = implode(' ', $data);
+                foreach ($job->rules as $rule) {
+                    $messages = $imap->getMessages();
 
-                        preg_match_all($rule->regex, $data, $matches, PREG_SET_ORDER, 0);
-                        if (is_array($matches) && !empty($matches)) {
-                            //Отправляем запрос
-                            $method = !empty($rule->webhook_method) ? $rule->webhook_method : 'GET';
-                            $url = $rule->webhook_url;
-                            $webhookData = $rule->webhook_data;
-                            if (!empty($webhookData)) {
-                                $webhookData = str_replace('#REGEX_MATCHES#', json_encode($matches), $data);
-                            }
+                    //Фильтр по отправителю
+                    if (!empty($rule->sender)) {
+                        $imap->filterEmailByFrom(
+                            $rule->sender,
+                            $messages
+                        );
+                    }
 
-                            $client = new Client();
+                    //Фильтр по заголовку
+                    if (!empty($rule->theme)) {
+                        $imap->filterEmailByTitle(
+                            $rule->theme,
+                            $messages
+                        );
+                    }
 
-                            switch ($method) {
-                                case 'GET':
-                                    $webhookData = mb_split("\n", $webhookData);
-                                    $url .= '?' . http_build_query($webhookData);
-                                    $client->get($url);
-                                    break;
+                    // Получим данные из текста сообщений
+                    if (!empty($rule->regex)) {
+                        foreach ($messages as $message) {
+                            $data = [
+                                mb_strtolower($message->getSubject()->toString()),
+                                mb_strtolower($message->getFrom()->toString()),
+                                mb_strtolower($message->getTextBody()),
+                            ];
+                            $data = implode(' ', $data);
 
-                                case 'POST':
-                                    $client->post(
-                                        $url,
-                                        [
-                                            'json' => $webhookData
-                                        ]
-                                    );
-                                    break;
+                            preg_match_all($rule->regex, $data, $matches, PREG_SET_ORDER, 0);
+                            if (is_array($matches) && !empty($matches)) {
+                                //Отправляем запрос
+                                $method = !empty($rule->webhook_method) ? $rule->webhook_method : 'GET';
+                                $url = $rule->webhook_url;
+                                $webhookData = $rule->webhook_data;
+                                if (!empty($webhookData)) {
+                                    $webhookData = str_replace('#REGEX_MATCHES#', json_encode($matches), $data);
+                                }
+
+                                switch ($method) {
+                                    case 'GET':
+                                        $webhookData = mb_split("\n", $webhookData);
+                                        $url .= '?' . http_build_query($webhookData);
+                                        $client->getAsync($url);
+                                        break;
+
+                                    case 'POST':
+                                        $client->postAsync(
+                                            $url,
+                                            [
+                                                'json' => $webhookData
+                                            ]
+                                        );
+                                        break;
+                                }
                             }
                         }
                     }
                 }
 
-
+                $job->last_run = Carbon::now();
+                $job->save();
+            } catch (\Throwable $exception) {
+                echo '<pre>';
+                print_r($exception->getMessage());
+                echo '</pre>';
             }
+
         })->everyMinute();
     }
 
